1fcb898fffd49c7cc6ab08a98caa9dde0365cff0
Sadik Shaikh <abdullahsadik00@gmail.com>
2024-10-07 00:15:53 +0530

    Index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,3 +4,27 @@ const database = require('./config/db');
 const cookieParser = require('cookie-parser')
 const cors = require('cors');
 const {cloudinaryConnect} = require('./config/cloudinary');
+const dotenv = require('dotenv').config();
+const PORT = process.env.PORT || 5000;
+app.use(cors({
+    origin: ['http://localhost:3000'],
+    credentials: true,
+}))
+
+app.use(express.json());
+
+app.use(cookieParser());
+
+app.get('/', (req, res) => {
+    res.status(200).json({
+        hasError: false,
+        message: 'Welcome to the Ed-Tech API',
+    })
+})
+
+app.listen(PORT, (req, res)=>{
+    console.log(`Server running on port ${PORT}`);
+    database.connect();
+    cloudinaryConnect();
+    console.log('Connected to MongoDB');
+})
