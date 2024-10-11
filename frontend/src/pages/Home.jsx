505c57d933a0b4845cd1986752d36dcebc4bdd78
Sadik Shaikh <abdullahsadik00@gmail.com>
2024-10-11 21:43:13 +0530

    Hero Section : WIP

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from 'react';
+import img from '../assets/bg-university.jpg';
+import img2 from '../assets/blob.png';
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div>
+      <div className="relative w-screen min-h-screen overflow-hidden font-inter">
+        {/* Background Image */}
+        <div
+          className="absolute inset-0 bg-cover bg-center"
+          style={{ backgroundImage: `url(${img})` }}
+        ></div>
 
-export default Home
+        {/* Overlay */}
+        <div className="absolute inset-0 bg-richblack-900 opacity-80"></div>
+
+        {/* Content */}
+        <div className="relative flex h-full text-white font-inter">
+          <div className="left w-1/2"></div>
+          <div className="right w-1/2 flex items-end justify-center h-full"> {/* Added h-full */}
+            <img src={img2} alt="Description of the image" className="mb-0" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
